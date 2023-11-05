a341dab061d3af428677bb8967408521179fe3a5
Mashiur Rahman <mashiur.dev@gmail.com>
2023-11-06 00:22:10 +0600

    (add) archive, page templates added (modified) index template update

--- functions.php
+++ functions.php
@@ -25,5 +25,34 @@ if (!function_exists("blogx_enqueue_scripts")) {
         wp_enqueue_style("blogx-style", get_template_directory_uri() . "/assets/style/main.css", array(), wp_get_theme()->get("version"));
     }
 }
-
 add_action("wp_enqueue_scripts", "blogx_enqueue_scripts");
+
+/**
+ * Customize Archive Page Title 
+ */
+
+if (!function_exists('blogx_rewrite_archive_page_title')) {
+    function blogx_rewrite_archive_page_title( $title ) {
+
+        if ( is_category() ) {
+
+            $post_text = $GLOBALS['wp_query']->post_count > 1 ? 'Posts' : 'Post';
+            $title = $title . "<span class='archive-post-count'>{$GLOBALS['wp_query']->post_count} $post_text</span>";
+
+        } elseif ( is_tag() ) {
+
+            $post_text = $GLOBALS['wp_query']->post_count > 1 ? 'Posts' : 'Post';
+            $title = $title . "<span class='archive-post-count'>{$GLOBALS['wp_query']->post_count} $post_text</span>";
+
+        } elseif ( is_author() ) {
+
+            $post_text = $GLOBALS['wp_query']->post_count > 1 ? 'Posts' : 'Post';
+            $title = $title . "<span class='archive-post-count'>{$GLOBALS['wp_query']->post_count} $post_text</span>";
+
+        }
+
+        return $title;
+
+    }
+}
+add_filter('get_the_archive_title', 'blogx_rewrite_archive_page_title');
